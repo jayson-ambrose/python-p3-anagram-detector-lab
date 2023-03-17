09487d6f0ce57c52df9fa7f181907d887fdbc03e
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-03-16 20:13:08 -0600

    lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word_letters = sorted([char for char in word])
+
+    def match(self, list):
+        match_list = []
+
+        for word in list:
+            if sorted([char for char in word]) == self.word_letters:
+                match_list.append(word)
+
+        return match_list
